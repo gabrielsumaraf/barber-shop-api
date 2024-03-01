7819f96149cbcfba310ab54c8534b0994e294824
Gabriel Sumara <gabrielsumarafortuna@gmail.com>
2024-03-01 19:14:18 -0300

    fixed user table migration

--- src/main/resources/db/migration/V1__create-users-table.sql
+++ src/main/resources/db/migration/V1__create-users-table.sql
@@ -1,10 +1,8 @@
-CREATE TYPE user_role  AS ENUM ('CUSTOMER', 'OWNER');
-
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
+    id UUID PRIMARY KEY UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    phone VARCHAR(11) NOT NULL,
+    phone VARCHAR(11) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role user_role NOT NULL
-)
+    role VARCHAR(10) NOT NULL CHECK (role IN ('OWNER', 'CUSTOMER')) DEFAULT ('CUSTOMER')
+);
